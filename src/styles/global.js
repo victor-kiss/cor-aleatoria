ca5167138a5809afe9c121ad5ff76144d3deb0f0
Victor Kiss <victorkiss10@gmail.com>
2021-03-16 19:27:06 -0300

    estilo global adicionado

--- src/styles/global.js
+++ src/styles/global.js
@@ -7,10 +7,14 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
         Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        box-sizing:border-box;
     }
     h1{
         font-size:4.0em;
         letter-spacing:1px;
+    }
+    body{
+        background-color:${props => props.theme.color}
     }
 `
 
